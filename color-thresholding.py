43b2de17d07f42978ef58def8b31f27a19dbb4a8
Rajeev Ravindran <razorrajeev@gmail.com>
2016-06-25 19:12:25 +0530

    Code Cleanup. Variable names updated. GUI improvements.

--- color-thresholding.py
+++ color-thresholding.py
@@ -6,47 +6,55 @@ cap = cv2.VideoCapture(0)
 cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH,640)
 cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT,480)
 
-point = numpy.zeros((50,50,3),numpy.uint8)
-point[:] = [64,164,67]
+extractedColorMat = numpy.zeros((50,50,3),numpy.uint8)
+extractedColorMat[:] = [64,164,67]
+
 def nothing(x):
     pass
 
-lower = [10,10,10]
-upper = [0,0,0]
-threshU = 5
-threshL = 5
+lowerRangeThreshold = [10,10,10]
+upperRangeThreshold = [0,0,0]
+upperTolerance = 5
+lowerTolerance = 5
 
-cv2.namedWindow('Trackbar')
-cv2.createTrackbar('threshL','Trackbar',0,100,nothing)
-cv2.createTrackbar('threshU','Trackbar',0,100,nothing)
+cv2.namedWindow('Control Window',flags=cv2.cv.CV_WINDOW_NORMAL)
+cv2.resizeWindow('Control Window',480,240)
+cv2.moveWindow('Control Window',0,0)
+cv2.namedWindow('Camera Feed',flags=cv2.cv.CV_WINDOW_NORMAL)
+cv2.resizeWindow('Camera Feed',330,240)
+cv2.moveWindow('Camera Feed',481,0)
+cv2.namedWindow('THRESHOLD',flags=cv2.cv.CV_WINDOW_NORMAL)
+cv2.moveWindow('THRESHOLD',0,280)
+cv2.createTrackbar('lowerTolerance','Control Window',10,100,nothing)
+cv2.createTrackbar('upperTolerance','Control Window',10,100,nothing)
 
 def clickEvent(event,x,y,flags,param):
     if event == cv2.EVENT_LBUTTONDOWN:
         print "Coordinates :",x,y
         color = image[(y,x)]
         print "clicked "+str(color)
-        point[:]=[color[0],color[1],color[2]]
+        extractedColorMat[:]=[color[0],color[1],color[2]]
 while 1:
-    threshL = cv2.getTrackbarPos('threshL','Trackbar')
-    threshU = cv2.getTrackbarPos('threshU','Trackbar')
+    lowerTolerance = cv2.getTrackbarPos('lowerTolerance','Control Window')
+    upperTolerance = cv2.getTrackbarPos('upperTolerance','Control Window')
     ret, image = cap.read()
-    cv2.imshow('color',point)
-    HSV_MAT = cv2.cvtColor(point,cv2.COLOR_BGR2HSV)
+    cv2.imshow('Control Window',extractedColorMat)
+    HSV_MAT = cv2.cvtColor(extractedColorMat,cv2.COLOR_BGR2HSV)
     HSV_COLOR = HSV_MAT[(8,8)]
     hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
-    lower = [HSV_COLOR[0]-threshL,HSV_COLOR[1]-threshL,HSV_COLOR[2]-threshL]
-    upper = [HSV_COLOR[0]+threshU,HSV_COLOR[1]+threshU,HSV_COLOR[2]+threshU]
+    lowerRangeThreshold = [HSV_COLOR[0]-lowerTolerance,HSV_COLOR[1]-lowerTolerance,HSV_COLOR[2]-lowerTolerance]
+    upperRangeThreshold = [HSV_COLOR[0]+upperTolerance,HSV_COLOR[1]+upperTolerance,HSV_COLOR[2]+upperTolerance]
     #dilation = cv2.dilate(erosion,kernel,iterations = 5)
-    #print "VALUES : ",lower[0],lower[1],lower[2],upper[0],upper[1],upper[2]
-    threshold = cv2.inRange(hsv,numpy.array([lower[0],lower[1],lower[2]]),numpy.array([upper[0],upper[1],upper[2]]))
-    kernel = numpy.ones((5,5),numpy.uint8)
-    erosion = cv2.erode(threshold,kernel,iterations = 3)
-    cv2.imshow('THRESHOLD',erosion)
-    cv2.setMouseCallback('IMAGE',clickEvent)
+    #print "VALUES : ",lowerRangeThreshold[0],lowerRangeThreshold[1],lowerRangeThreshold[2],upperRangeThreshold[0],upperRangeThreshold[1],upperRangeThreshold[2]
+    threshold = cv2.inRange(hsv,numpy.array([lowerRangeThreshold[0],lowerRangeThreshold[1],lowerRangeThreshold[2]]),numpy.array([upperRangeThreshold[0],upperRangeThreshold[1],upperRangeThreshold[2]]))
+    #kernel = numpy.ones((5,5),numpy.uint8)
+    #erosion = cv2.erode(threshold,kernel,iterations = 3)
+    cv2.imshow('THRESHOLD',threshold)
+    cv2.setMouseCallback('Camera Feed',clickEvent)
     #a,b,c,d = cv2.boundingRect(erosion)
     #print a,b,c,d
     #cv2.rectangle(image,(a,b),(a+c,b+d),(0,0,255),1)
-    cv2.imshow('IMAGE',image)
+    cv2.imshow('Camera Feed',image)
     if(cv2.waitKey(1)==32):break
 cap.release()
 cv2.destroyAllWindows()
